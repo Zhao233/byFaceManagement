1908d5f92b5f9a9dd4d63f77e8df27f01000a2cf
zhaoxu <9503071759@qq.com>
2018-06-10 23:42:11 +0800

    完成user页面的展示，可以从数据库中获取user信息b并展示出来

--- src/main/java/com/example/demo/controller/UserManagementController.java
+++ src/main/java/com/example/demo/controller/UserManagementController.java
@@ -6,9 +6,12 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller()
 @RequestMapping(value = "/console/user_management")
@@ -26,12 +29,13 @@ public class UserManagementController {
 
 
     @ResponseBody
-    @RequestMapping(value = "/all", produces = {"application/json;charset=UTF-8"})
-    public List<User> findAllUser(){
+    @RequestMapping(value = "/all", produces = {"application/json;charset=UTF-8"}, method = RequestMethod.GET)
+    public Map<String,Object> findAllUser(){
+        Map<String,Object> map = new HashMap<>();
 
-        return userService.getAllUser();
+        map.put("data",userService.getAllUser());
+        return map;
     }
-
 
     @RequestMapping("/test")
     @ResponseBody
