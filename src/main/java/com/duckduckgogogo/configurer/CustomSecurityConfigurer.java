3343fca5fb95e324ce0a97429d590a0d407740e6
zhaoxu <9503071759@qq.com>
2018-07-05 13:45:52 +0800

    完成多级菜单

--- src/main/java/com/duckduckgogogo/configurer/CustomSecurityConfigurer.java
+++ src/main/java/com/duckduckgogogo/configurer/CustomSecurityConfigurer.java
@@ -54,7 +54,7 @@ public class CustomSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/style/**", "/javascript/**", "/fonts/**", "/api/**", "/console/**", "/images/**", "/voices/**").permitAll() //
+                .antMatchers("/", "/style/**", "/javascript/**", "/fonts/**", "/api/**", "/console/**", "/images/**", "/voices/**", "/image/**").permitAll() //
                 .anyRequest().authenticated() //
                 .and().formLogin().loginPage("/login").successForwardUrl("/logged").failureUrl("/?error").permitAll() //
                 .and().logout().logoutUrl("/logout").logoutSuccessUrl("/").permitAll(); //
